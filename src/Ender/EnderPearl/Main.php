814f06fcb860da631a97f29d907068baed272c75
AdminConfirmed <50870119+AdminConfirmed@users.noreply.github.com>
2019-05-21 17:27:52 +0100

    Add files via upload

--- src/Ender/EnderPearl/Main.php
+++ src/Ender/EnderPearl/Main.php
@@ -2,9 +2,13 @@
 
 namespace Ender\EnderPearl;
 
+use pocketmine\item\Item;
 use pocketmine\utils\Config;
 use pocketmine\event\Listener;
+use pocketmine\command\Command;
+use pocketmine\utils\TextFormat;
 use pocketmine\plugin\PluginBase;
+use pocketmine\command\CommandSender;
 use pocketmine\entity\projectile\EnderPearl;
 use pocketmine\event\entity\ProjectileLaunchEvent;
 
@@ -19,6 +23,50 @@ class Main extends PluginBase implements Listener {
         $this->saveResource("config.yml");  
 
 		$this->config = new Config($this->getDataFolder()."config.yml", Config::YAML);
+
+    }
+
+    public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool {
+
+        if($command->getName() === "pearlcooldown") {
+
+            if(!$sender->hasPermission("pearlcooldown.change")) {
+
+                $sender->sendMessage(TextFormat::RED . "You do not have permissions to use this command");
+
+                return false;
+
+            }
+
+            if(isset($args[0])) {
+
+                $newcooldown = $args[0];
+
+                if(!is_numeric($newcooldown)){
+
+                    $sender->sendMessage("Cooldown must be numeric");
+
+                    return false;
+
+                }
+
+                $oldcooldown = $this->config->get("cooldown");
+
+                $this->config->set("cooldown", $newcooldown);
+
+                $this->config->save();
+
+                $sender->sendMessage("Cooldown set to $newcooldown from $oldcooldown");
+
+                return false;
+
+            }
+
+            $sender->sendMessage("Usage: /pearlcooldown <seconds>");
+
+        }
+
+        return true;
 
     }
 
@@ -44,7 +92,11 @@ class Main extends PluginBase implements Listener {
 
                     $time = time() - $this->pearlcd[strtolower($player->getName())];
 
-                    $player->sendMessage("You cannot use a pearl until " . ($cooldown - $time) . " seconds");
+                    $message = $this->config->get("message");
+
+                    $message = str_replace("{cooldown}", ($cooldown - $time), $message);
+
+                    $player->sendMessage($message);
 
                 } else {
 
